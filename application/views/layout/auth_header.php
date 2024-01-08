84d063af93a42593ce3d10a6a2209f6c999fdf0a
RanggaAfrian <rangga22ti@mahasiswa.pcr.ac.id>
2024-01-09 05:38:17 +0700

    Initial commit

---
+++ application/views/layout/auth_header.php
@@ -0,0 +1,31 @@
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>MSA PRINNTING</title>
+    <!-- Favicon-->
+    <link rel="icon" type="<?= base_url('assets/') ?>MSA.png" href="" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="<?= base_url('assets/') ?>css/styles.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand" href="#!">MSA PRINTING</a>
+            </div>
+        </div>
+    </nav>
+    <!-- Header-->
+    <header class="bg-dark py-5">
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="text-center text-white">
+                <img src="<?= base_url('assets/')?>CC.png" alt="" width="15%" height="10%">
+                <h1 class="display-4 fw-bolder">WELCOME</h1>
+                <p class="lead fw-normal text-white-50 mb-0">YOUR BUSINESS SOLUTIONS</p>
+            </div>
+    </header>
---
+++ application/views/layout/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>MSA PRINTING</title>
+    <!-- Favicon-->
+    <link rel="icon" type="<?= base_url('assets/') ?>MSA.png" href="" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="<?= base_url('assets/') ?>css/styles.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand" href="#">
+                <img src="<?= base_url('assets/')?>print.png" alt="" width="60" height="60"></a>
+            <a class="navbar-brand" href="#!">MSA PRINTING</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                    </form>
+            </div>
+        </div>
+    </nav>
+    <!-- Header-->
+    <header class="bg-dark py-5">
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="text-center text-white">
+                <img src="<?= base_url('assets/')?>CC.png" alt="" width="15%" height="10%">
+                <h1 class="display-4 fw-bolder">WELCOME</h1>
+                <p class="lead fw-normal text-white-50 mb-0">YOUR BUSINESS SOLUTIONS</p>
+
+
+            </div>
+        </div>
+    </header>
